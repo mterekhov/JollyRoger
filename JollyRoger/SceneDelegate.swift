b6191e1ff2232a1e4783ec4bd928781e3c66a219
Michael Terekhov <m.terekhov@icloud.com>
2024-03-13 14:12:16 +0300

    initial commit

---
+++ JollyRoger/SceneDelegate.swift
@@ -0,0 +1,41 @@
+//
+//  SceneDelegate.swift
+//  JollyRoger
+//
+//  Created by cipher on 13.03.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowsScene = (scene as? UIWindowScene) else {
+            return
+        }
+        
+//        let viewController = DJDreamsJournalVC()
+//        viewController.dreamsService = DJDreamsService((UIApplication.shared.delegate as? AppDelegate)?.coreDataService)
+        let navvc = UINavigationController(rootViewController: ViewController())
+        
+        window = UIWindow(frame: windowsScene.coordinateSpace.bounds)
+        window?.backgroundColor = .white
+        window?.windowScene = windowsScene
+        window?.rootViewController = navvc
+        window?.makeKeyAndVisible()
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        // Called as the scene transitions from the foreground to the background.
+        // Use this method to save data, release shared resources, and store enough scene-specific state information
+        // to restore the scene back to its current state.
+
+        // Save changes in the application's managed object context when the application transitions to the background.
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+    }
+
+
+}
+
---
+++ JollyRoger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JollyRoger
+//
+//  Created by cipher on 13.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func loadView() {
+        super.loadView()
+        
+        view.backgroundColor = .cyan
+    }
+    
+}
+
